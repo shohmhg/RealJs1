e5dd6eeca5fc1afa295618f7a38261fe7946c977
shohmhg <shohmhg@Naver.com>
2019-07-30 05:32:46 +0900

    go

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const canvas = document.getElementById("jsCanvas");
+const canvas = document.getElementById("jsCanvas"),
+    ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 700;
 
 let painting = false;
 
@@ -7,21 +11,28 @@ function stopPainting(){
 }
 
 function mouseMove(event){
-    console.log(event);
+    let x = event.offsetX;
+    let y = event.offsetY;
+
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        console.log(ctx.moveTo);
+    }
+    console.log(x, y);
 }
 
 function mouseDown(event){
-    painting = ture;
-}
-
-function mouseUp(){
-    stopPainting();
+    painting = true;
 }
 
 function init(){
     canvas.addEventListener("mousemove", mouseMove);
     canvas.addEventListener("mousedown", mouseDown);
-    canvas.addEventListener("mouseup", mouseUp);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
 
